0248d7dbfdc7d939a818b0a31b7bc8b675b20e2a
geoffembuscad0 <geoff.embuscado@yahoo.com.ph>
2024-04-10 12:07:21 +0800

    Remove duplicate code

--- lib/pages/settings_page.dart
+++ lib/pages/settings_page.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_geoff_notes_app/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
